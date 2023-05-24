e7fa8963de078a3097ba4d0676c3c233536ed8f3
marcos carvalho <marcosantcarvalho99@gmail.com>
2023-05-24 14:41:04 -0300

    criação da pagina suporte html e css

--- src/app/pages/suporte/suporte.component.ts
+++ src/app/pages/suporte/suporte.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-suporte',
   templateUrl: './suporte.component.html',
   styleUrls: ['./suporte.component.css']
 })
-export class SuporteComponent {
+export class SuporteComponent implements OnInit {
+
+  supImgLogin:string = '/assets/img/loginusuario.png';
+  supImgCad:string = '/assets/img/cadastro.png';
+  supImgVendas:string = '/assets/img/vendas.png';
+
+
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
