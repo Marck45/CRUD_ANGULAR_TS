0c07449ee5ec8b024ba7082f61243d054193a733
marcos carvalho <marcosantcarvalho99@gmail.com>
2023-08-21 22:06:06 -0300

    style: estilos e layoouts dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
     path: 'produtos', component: ProdutosComponent
   },
   {
-    path: 'Produtos', component:ProductsDetailsComponent
+    path: 'produtosDetails', component:ProductsDetailsComponent
   }
 
 ];
