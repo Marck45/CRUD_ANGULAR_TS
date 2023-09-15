6d5625dc1d4403f3ef92e462675496c00e5ca45a
marcos carvalho <marcosantcarvalho99@gmail.com>
2023-09-14 21:35:39 -0300

    feat: creat model, class and service

---
+++ src/app/class/customer/customer.ts
@@ -0,0 +1,10 @@
+import { Customer } from "src/app/models/customer";
+
+
+export class CustomerClass implements Customer {
+      name : string = '';
+      email: string = '';
+      phone: Number = 0;
+      address: string = ''
+      notification: Boolean = false;
+}
---
+++ src/app/models/customer.ts
@@ -0,0 +1,7 @@
+export interface Customer {
+  name : string;
+  email: string;
+  phone: Number;
+  address: string;
+  notification: Boolean;
+}
