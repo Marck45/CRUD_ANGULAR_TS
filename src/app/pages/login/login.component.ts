09fe0bbfc4e23a376a0c87a383510aeb7b2334ec
marcos carvalho <marcosantcarvalho99@gmail.com>
2024-01-29 23:10:36 -0300

    feat: ajuste do logo e botão de teste

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Login } from 'src/app/models/login';
-import { LoadingService } from 'src/app/service/loading.service';
-import { NotificationService } from 'src/app/service/notification/notification.service';
-import { Router } from '@angular/router';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
 
@@ -12,23 +7,10 @@ import { DOCUMENT } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
+logoImg = "./assets/img/logo.png"
 
-  imgLogin: string = '/assets/img/img-login.jpg';
-  googleImg: string = 'assets/img/gmail.png';
-  faceImg: string = '/assets/img/facebookVSG.svg';
-
-  login = {} as Login;
-  logins: Login[] = [];
-
-  formLogin!: FormGroup;
-
-  constructor( private loadingService: LoadingService,
-    private notificationService: NotificationService, private router: Router, public auth: AuthService, @Inject(DOCUMENT) public document: Document) { }
-
-  ngOnInit(): void {;
-
-  }
+  constructor(public auth: AuthService, @Inject(DOCUMENT) public document: Document) { }
 
 }
 
