6b5fbbeee6b8cdf261d5743a63a02e7194afe230
marcos carvalho <marcosantcarvalho99@gmail.com>
2024-02-01 22:33:10 -0300

    feat: front do form de lead criado

--- src/app/component/page-contact/page-contact.component.ts
+++ src/app/component/page-contact/page-contact.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-page-contact',
   templateUrl: './page-contact.component.html',
   styleUrls: ['./page-contact.component.css']
 })
-export class PageContactComponent {
+export class PageContactComponent implements OnInit {
+
+  formLeads!: FormGroup;
+
+  textButton = 'Teste Gratís';
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.createForm();
+    this.textButton;
+  }
+
+  async createForm() {
+    this.formLeads = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      phone: new FormControl(),
+    });
+  }
 
 }
